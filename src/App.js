9293d7e6c702a48172b949662d2f0e93438c6f6f
mony-io <rysarakmony6101@gmail.com>
2022-10-03 16:06:29 +0700

    React order coffee app

---
+++ src/App.js
@@ -0,0 +1,30 @@
+import { useState } from 'react';
+
+import Header from './components/Layout/Header';
+import Meals from './components/Meals/Meals';
+import Cart from './components/Cart/Cart';
+import CartProvider from './store/CartProvider';
+
+function App() {
+  const [cartIsShown, setcartIsShown] = useState(false);
+
+  const showCartHandler = () => {
+    setcartIsShown(true);
+  }
+
+  const hideCartHandler = () => {
+    setcartIsShown(false);
+  }
+
+  return (
+    <CartProvider>
+      {cartIsShown && <Cart onClose={hideCartHandler} />}
+      <Header onShowCart={showCartHandler} />
+      <main>
+        <Meals />
+      </main>
+    </CartProvider>
+  );
+}
+
+export default App;
